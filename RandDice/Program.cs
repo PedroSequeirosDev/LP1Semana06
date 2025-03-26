8a4aab8270569796635d8e92edc1d2f5e39af170
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-26 23:25:25 +0000

    add dice and seed args and conversions

---
+++ RandDice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string Dice = args[0];
+            int rolls = int.Parse(Dice);
+
+            string seed = args[1];
+            int seedConverted = int.Parse(seed);
+
+     
