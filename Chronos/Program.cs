5a1763ace45351de9886e8f9a945c0b9968ee9f5
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-26 22:10:54 +0000

    add chrono1 and chrono2

---
+++ Chronos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+
+            Stopwatch crono2 = new Stopwatch();
+
+
+        }
+    }
+}
