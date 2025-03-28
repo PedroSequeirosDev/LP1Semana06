e1e24a1347126075a468528941d510e49c321586
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-28 16:53:38 +0000

    Replace program.cs with the one provided

--- MyRoguelike/Program.cs
+++ MyRoguelike/Program.cs
@@ -1,37 +1,39 @@
 ﻿using System;
-using System.Globalization;
 
-namespace MyGame
+namespace MyRoguelike
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            string AmountofEnemies = args[0];
-            int EnemiesConverted = int.Parse(AmountofEnemies);
+            Level lvl = new Level(125, Toughness.Nightmare);
 
-            Enemy[] AllEnemies = new Enemy[EnemiesConverted];
+            lvl.SetEnemyInRoom(0, new Enemy("Urdnot Wrex"));
+            lvl.SetEnemyInRoom(120, new Enemy("Commander Shepard"));
+            lvl.SetEnemyInRoom(41, new Enemy("Liara T'Soni"));
+            lvl.SetEnemyInRoom(13, new Enemy("Tali'Zorah"));
+            lvl.SetEnemyInRoom(59, new Enemy("Garrus Vakarian"));
+            lvl.SetEnemyInRoom(99, new Enemy("Joker"));
 
-            for (int NumberofEnemies = 0; NumberofEnemies < EnemiesConverted; NumberofEnemies++)
-            {
-                Console.Write($"Nome do inimigo {NumberofEnemies + 1}: ");
-                AllEnemies[NumberofEnemies] = new Enemy(Console.ReadLine());
-            }
+            Console.WriteLine($"Toughness: {lvl.GetToughness()}");
 
-            foreach (Enemy enemy in AllEnemies)
-            {
-                enemy.TakeDamage(50);
-                enemy.PickupPowerUp(PowerUp.Shield, 50);
-                enemy.PickupPowerUp(PowerUp.Health, 115);
-                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
+            Console.WriteLine($"Number of rooms: {lvl.GetNumRooms()}");
 
+            Console.WriteLine($"Number of enemies: {lvl.GetNumEnemies()}");
 
-            }
+            lvl.PrintEnemies();
 
-            Console.WriteLine($"Amount of PowerUps: {Enemy.HowManyPowerUps}");
-
-
-
+            // Este programa mostra o seguinte no ecrã:
+            //
+            // Toughness: Nightmare
+            // Number of rooms: 125
+            // Number of enemies: 6
+            // Zeroth room: Urdnot Wrex
+            // Thirteenth room: Tali'Zorah
+            // Forty-First room: Liara T'Soni
+            // Fifty-Ninth room: Garrus Vakarian
+            // Ninety-Ninth room: Joker
+            // Hundred and Twentieth room: Commander Shepard
         }
     }
 }
